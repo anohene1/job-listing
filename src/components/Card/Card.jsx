75c5eea3d59f0bf3e8975821a80d06a9900cd6ea
anohene1 <anoheneisaac@gmail.com>
2022-02-09 17:30:56 +0000

    fix filtering to meet requirement

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,11 +4,13 @@ import Tag from "../Tag/Tag";
 import Badge from "../Badge/Badge";
 
 function Card({ item, onClick }) {
-  const tags = item.languages.map((language, index) => (
-    <Tag key={index} onClick={(name) => onClick(name)}>
-      {language}
-    </Tag>
-  ));
+  const tags = [item.role, item.level, ...item.languages, ...item.tools].map(
+    (category, index) => (
+      <Tag key={index} onClick={(name) => onClick(name)}>
+        {category}
+      </Tag>
+    )
+  );
   const style = {
     borderLeft: item.featured ? "5px solid hsl(180, 29%, 50%)" : "none",
   };
